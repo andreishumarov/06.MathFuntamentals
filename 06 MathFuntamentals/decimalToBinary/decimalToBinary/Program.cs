7811034adc25d3682eb8afab9e89713bd51aa36e
andreishumarov <114391036+andreishumarov@users.noreply.github.com>
2022-11-24 11:55:04 +0200

    Add files via upload

---
+++ 06 MathFuntamentals/decimalToBinary/decimalToBinary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace decimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if (!IsDigitsOnly(input))
+            {
+                Console.WriteLine("Стринга не е число");
+                return;
+            }
+            int number = int.Parse(input);
+            int[] result = new int[32];
+            int i = 0;
+            for (i = 0; number > 0; i++)
+            {
+                result[i] = number % 2;
+                number = number / 2;
+            }
+            Console.Write("Binary of the given number= ");
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(result[i]);
+            }
+            Console.WriteLine();
+        }
+        static bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+            return true;
+        }
+    }
+}
